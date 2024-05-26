08b5f4284c3959ce849263d0729296a15dc3c297
Nahuel Roldan <nahuel.roldan@hotmail.com.ar>
2024-05-26 14:50:55 -0300

    se agregan validaciones al formulario

--- script/script.js
+++ script/script.js
@@ -6,7 +6,7 @@ btnLogin.addEventListener('click',activarFormulario);
 function activarFormulario(){
     const mostrarLogin = document.querySelector('.form');
 
-    mostrarLogin.classList.toggle('ocultar');
+    mostrarLogin.classList.toggle('mostrar');
 
 }
 
@@ -18,7 +18,7 @@ const formulario = document.querySelector('.form');
 formulario.addEventListener('submit',evento =>{
 
     // cancelar envio de datos por defecto
-    evento.defaultPrevented();
+    evento.preventDefault();
 
     // valiar los campos del formulario
     if (validarFomulario()){
@@ -26,9 +26,31 @@ formulario.addEventListener('submit',evento =>{
         formulario.submit();
 
     }
-
-
 });
+
+function validarFomulario(){
+
+    //obtener los valores de los campos
+    const email = document.getElementById('exampleInputEmail1').value ;
+    const password = document.getElementById('exampleInputPassword1').value ;
+
+    if(email === ''){
+        mostrarError('email','Por favor ingrese el mail')
+        return false;
+    }
+
+    if(password === ''){
+        mostrarError('password','Por favor ingrese la contraseña')
+        return false;
+    }
+
+    // if(!validarEmail(email)){
+    //     mostrarError('email','Por favor ingresa un correo electronico valido')
+    //     return false
+    // }
+
+    return true;
+}
 
 // funcion para mostrar mensaje de error
 function mostrarError(campo,mensaje){
@@ -37,4 +59,10 @@ function mostrarError(campo,mensaje){
 }
 
 
-// funcion para validar correo electronico
+// funcion para validar correo electronico utilizando una expresion regular
+function validarEmail(email){
+    const expresionRegular = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return expresionRegular.test(email);
+
+}
+
