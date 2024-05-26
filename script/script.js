87358d748ddd7ea9f2cc30a689f2a5ebe1bd468b
Nahuel Roldan <nahuel.roldan@hotmail.com.ar>
2024-05-26 03:34:04 -0300

    se empiezan a validar los campos

--- script/script.js
+++ script/script.js
@@ -9,3 +9,32 @@ function activarFormulario(){
     mostrarLogin.classList.toggle('ocultar');
 
 }
+
+// ------------LOGIN-------------
+// obtener el formulario
+const formulario = document.querySelector('.form');
+
+// agregar un evento de escucha para cuando se envie el formulario
+formulario.addEventListener('submit',evento =>{
+
+    // cancelar envio de datos por defecto
+    evento.defaultPrevented();
+
+    // valiar los campos del formulario
+    if (validarFomulario()){
+        // si todos los campos son validos, enviar el formulario
+        formulario.submit();
+
+    }
+
+
+});
+
+// funcion para mostrar mensaje de error
+function mostrarError(campo,mensaje){
+    const campoError = document.getElementById(`${campo}-error`);
+    campoError.innerText = mensaje;
+}
+
+
+// funcion para validar correo electronico
